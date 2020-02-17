be85ce4af56c1690d92ee954ca51758bf0794ed7
Jhanssen Favaro <jhanssenfavaro@gmail.com>
2020-02-17 02:31:29 -0300

    Create app.py
    Need redis-server, flask and python rq to be run

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request
+import redis
+from rq import Connection, Worker, Queue
+import time
+import multiprocessing
+
+r = redis.Redis()
+app = Flask(__name__)
+
+def list_workers():
+    def serialize_queue_names(worker):
+        return [q.name for q in worker.queues]
+
+    workers = [dict(name=worker.name, queues=serialize_queue_names(worker),
+        state=worker.get_state()) for worker in Worker.all()]
+    return dict(workers=workers)
+
+def runworker(queuename):
+    #Burst mode kills the worker once it has completed its jobs
+    qs=[queuename]
+    with Connection(r):
+        if Worker.find_by_key('rq:worker:'+queuename):
+            workerInstance = Worker.find_by_key('rq:worker:name')
+            print("Worker for queue "+queuename+" is already running")
+            print("Current amount of running jobs: ")
+        else:
+            print("worker was not alive, starting")
+            multiprocessing.Process(target=Worker(qs,connection=r,name=queuename).work, kwargs={'burst': False}).start()
+            # print("Workers: "+str(list_workers()))
+            #print("worker pid: "+multiprocessingstr(.Process(target=Worker)(qs).work).pid())
+
+def background_task(n):
+
+    delay = 5
+
+    print("Task running")
+    print(f"Simulating {delay} second delay")
+    time.sleep(delay)
+    print("Task complete")
+    return len(n)
+
+
+@app.route("/workers")
+def get_workers():
+    queuename = request.args.get("qname")
+    # Count the number of workers in this Redis connection
+    workersRedisCount = Worker.count(connection=r)
+    print("Current number of workers in this Redis: "+ str(workersRedisCount))
+
+    # Count the number of workers for a specific queue
+    queue = Queue(queuename, connection=r)
+    q_len = len(queue)
+    workersQueueInstance = Worker.all(queue=queue)
+    print("Current number of started workers for "+queuename+" equal to "+str(workersQueueInstance))
+    return f"Current Tasks: {q_len} tasks in the queue and total of {workersRedisCount}  workers."
+
+@app.route("/task")
+def add_task():
+    
+    if request.args.get("n"):
+        runworker(request.args.get("qname"))
+        q = Queue(request.args.get("qname"),connection=r)
+        job = q.enqueue(background_task, request.args.get("n"))
+        
+        q_len = len(q)
+
+        return f"Task {job.id} added to queue at {job.enqueued_at}. {q_len} tasks in the queue \n args: {job.args}"
+    
+    return "No value for n"
+
+if __name__ == "__main__":
+    app.run()
+    
+#Workers will read enqueued jobs
